ca021d1f35eb81e1997c328cd7f489acc4c7a4e3
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2024-02-25 10:39:37 -0300

    Initial commit

---
+++ src/FinancialHub.Common/Models/BaseModel.cs
@@ -0,0 +1,14 @@
+﻿namespace FinancialHub.Common.Models
+{
+    public abstract class BaseModel
+    {
+        public Guid? Id { get; set; }
+
+        protected BaseModel() { }
+
+        protected BaseModel(Guid? id)
+        {
+            Id = id;
+        }
+    }
+}
